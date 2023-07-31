f8a34b56f4bc491a8fd2537e1e91d26df4484b80
Cristian Andres <cristianvcx188@gmail.com>
2023-07-30 22:12:16 -0400

    digitic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,34 @@
 import React from "react";
-
+import { NavLink, Link } from "react-router-dom";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <header className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div class="row">
+            <div className="col-6">
+              <p className="text-white mb-0">
+                Envío gratis a partir de $100 y devoluciones gratis
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Linea directa:{" "}
+                <a className="text-white" href="tel:+56 9264954234">
+                  +56 9264954234
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row">{/*26:04*/}</div>
+        </div>
+      </header>
+    </>
+  );
 };
-//18:04
+
 export default Header;
